60d9663bdd08e49513e9fd6a862524b59ad78e79
ReiniVanBerch <vincent.berchi@outlook.com>
2025-04-25 00:52:35 +0200

    Preparing being able to export it

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,11 +6,9 @@ module tech.morbit {
     requires java.desktop;
     requires java.sql;
 
-
     exports tech.morbit.App;
     exports tech.morbit.Character;
     exports tech.morbit.Exception;
-    exports tech.morbit.Tools;
     exports tech.morbit.Quality;
     exports tech.morbit.Tag;
     exports tech.morbit.Functional;
