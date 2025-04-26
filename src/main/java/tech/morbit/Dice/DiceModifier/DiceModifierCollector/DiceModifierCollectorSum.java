fe126631cd11aaf804891f1762359666784ce3a6
ReiniVanBerch <vincent.berchi@outlook.com>
2025-04-26 23:30:21 +0200

    Implemented Sum

--- src/main/java/tech/morbit/Dice/DiceModifier/DiceModifierCollector/DiceModifierCollectorSum.java
+++ src/main/java/tech/morbit/Dice/DiceModifier/DiceModifierCollector/DiceModifierCollectorSum.java
@@ -1,4 +1,30 @@
 package tech.morbit.Dice.DiceModifier.DiceModifierCollector;
 
-public class DiceModifierCollectorSum {
+import tech.morbit.Dice.ConditionModifier.ConditionModifier;
+import tech.morbit.Dice.ConditionModifier.ConditionModifierTrue;
+
+import java.util.ArrayList;
+
+public class DiceModifierCollectorSum implements DiceModifierCollector {
+
+    ConditionModifier conditionModifier = new ConditionModifierTrue();
+
+    public DiceModifierCollectorSum(){}
+    public DiceModifierCollectorSum(ConditionModifier conditionModifier){
+        this.conditionModifier = conditionModifier;
+    }
+
+    @Override
+    public int apply(ArrayList<Integer> input) {
+        int result = sum(input);
+        if(conditionModifier.check(result)){
+            return result;
+        } else{
+            return 0;
+        }
+    }
+
+    public int sum(ArrayList<Integer> input) {
+        return input.stream().mapToInt(i -> i).sum();
+    }
 }
