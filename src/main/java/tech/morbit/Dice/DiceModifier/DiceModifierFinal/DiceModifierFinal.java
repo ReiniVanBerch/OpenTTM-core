7d18230069c4e80584a973c03f43866749b30877
ReiniVanBerch <vincent.berchi@outlook.com>
2025-04-26 23:11:22 +0200

    DiceModifierCollectprPre

--- src/main/java/tech/morbit/Dice/DiceModifier/DiceModifierFinal/DiceModifierFinal.java
+++ src/main/java/tech/morbit/Dice/DiceModifier/DiceModifierFinal/DiceModifierFinal.java
@@ -35,8 +35,8 @@ public abstract class DiceModifierFinal extends DiceModifier {
 
     public abstract int apply(int collected);
 
-    public boolean check(int collected){
-        return this.conditionModifier.check(collected);
+    public boolean check(int check){
+        return this.conditionModifier.check(check);
     }
 
 }
