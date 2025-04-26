02f44214b5d64bc359b6d14472c79be78480ff39
ReiniVanBerch <vincent.berchi@outlook.com>
2025-04-26 23:48:01 +0200

    DiceModifierCollectorAverage worked on

--- src/main/java/tech/morbit/Dice/DiceModifier/DiceModifierCollector/DiceModifierCollectorAverage.java
+++ src/main/java/tech/morbit/Dice/DiceModifier/DiceModifierCollector/DiceModifierCollectorAverage.java
@@ -1,4 +1,32 @@
 package tech.morbit.Dice.DiceModifier.DiceModifierCollector;
 
-public class DiceModifierCollectorAverage {
+import tech.morbit.Dice.ConditionModifier.ConditionModifier;
+import tech.morbit.Dice.ConditionModifier.ConditionModifierTrue;
+
+import java.util.ArrayList;
+
+public class DiceModifierCollectorAverage implements DiceModifierCollector {
+
+    ConditionModifier conditionModifier = new ConditionModifierTrue();
+
+    public DiceModifierCollectorAverage() {}
+
+    public DiceModifierCollectorAverage(ConditionModifier conditionModifier) {
+        this.conditionModifier = conditionModifier;
+    }
+
+    @Override
+    public int apply(ArrayList<Integer> input) {
+        int result = average(input);
+        if (conditionModifier.check(result)) {
+            return result;
+        } else {
+            return 0;
+        }
+    }
+
+    public int average(ArrayList<Integer> input) {
+        int sum = input.stream().mapToInt(i -> i).sum();
+        return sum / input.size();
+    }
 }
